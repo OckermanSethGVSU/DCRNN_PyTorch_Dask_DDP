8440002db278ae4b93d70de20963a9ed4c7a1453
Seth Ockerman <sockerman@cs.wisc.edu>
2024-07-17 13:00:44 -0500

    should shuffle batches - not individual items in x_train

--- lib/utils.py
+++ lib/utils.py
@@ -186,7 +186,7 @@ def load_dataset(dataset_dir, batch_size, test_batch_size=None, **kwargs):
     for category in ['train', 'val', 'test']:
         data['x_' + category][..., 0] = scaler.transform(data['x_' + category][..., 0])
         data['y_' + category][..., 0] = scaler.transform(data['y_' + category][..., 0])
-    data['train_loader'] = DataLoader(data['x_train'], data['y_train'], batch_size, shuffle=True)
+    data['train_loader'] = DataLoader(data['x_train'], data['y_train'], batch_size, shuffle=False)
     data['val_loader'] = DataLoader(data['x_val'], data['y_val'], test_batch_size, shuffle=False)
     data['test_loader'] = DataLoader(data['x_test'], data['y_test'], test_batch_size, shuffle=False)
     data['scaler'] = scaler
